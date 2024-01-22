1a736dd35a4b4c3363dfd67335e43b648c445be4
Norbert POINTU <norbert_pointu@hotmail.com>
2024-01-22 17:15:40 +0000

    exercide 1

--- workspaces/zenika-ng-website/src/app/app.component.ts
+++ workspaces/zenika-ng-website/src/app/app.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { Product } from './product/product.types';
+import { ProductComponent } from "./product/product.component";
+
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MenuComponent, ProductComponent]
 })
 export class AppComponent {
   title = 'my first component';
+
+  products: Productc =[
+    {
+      "id": "welsch",
+      "title": "Coding the welsch",
+      "description": "Tee-shirt col rond - Homme",
+      "photo": "/assets/coding-the-welsch.jpg",
+      "price": 20,
+      "stock": 2
+    },
+    {
+      "id": "world",
+      "title": "Coding the world",
+      "description": "Tee-shirt col rond - Homme",
+      "photo": "/assets/coding-the-world.jpg",
+      "price": 18,
+      "stock": 2
+    },
+    {
+      "id": "vador",
+      "title": "Duck Vador",
+      "description": "Tee-shirt col rond - Femme",
+      "photo": "/assets/coding-the-stars.jpg",
+      "price": 21,
+      "stock": 2
+    },
+    {
+      "id": "snow",
+      "title": "Coding the snow",
+      "description": "Tee-shirt col rond - Femme",
+      "photo": "/assets/coding-the-snow.jpg",
+      "price": 19,
+      "stock": 2
+    }
+  ]
 }
